73a746887f019d817219cb12c5b8a5364d3b454f
Mahimakumari711 <mahimakumari643899@gmail.com>
2026-05-28 14:40:40 +0530

    Create Question-4.kt
    Write a program to reverse a given number using loops.

---
+++ Android-Basic-Progams/Question-4.kt
@@ -0,0 +1,17 @@
+fun main() {
+    var number = 123
+    var reverse = 0
+
+    while (number != 0) {
+        val digit = number % 10
+        reverse = reverse * 10 + digit
+        number /= 10
+    }
+
+    println("Reversed number = $reverse")
+}
+
+
+Output :
+
+Reversed number = 321
