589e536f5e5c5d59e6d18dec6934a50b52edeb35
Mahimakumari711 <mahimakumari643899@gmail.com>
2026-05-28 14:32:28 +0530

    Create Question-2.kt
    Write a program to check whether a given number is even or odd.

---
+++ Android-Basic-Progams/Question-2.kt
@@ -0,0 +1,14 @@
+fun main() {
+    val number = 13
+
+    if (number % 2 == 0) {
+        println("$number is Even")
+    } else {
+        println("$number is Odd")
+    }
+}
+
+
+Output :
+
+13 is Odd
