014dab95d4bca711527179b88c91b09ed7635360
Mahimakumari711 <mahimakumari643899@gmail.com>
2026-05-28 14:52:54 +0530

    Create Question-11.kt
    Write a Kotlin program to find the sum of digits of a number.

---
+++ Android-Basic-Progams/Question-11.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var number = 1234
+    var sum = 0
+
+    while (number != 0) {
+        val digit = number % 10
+        sum += digit
+        number /= 10
+    }
+
+    println("Sum of digits = $sum")
+}
+
+Output :
+
+Sum of digits = 10
